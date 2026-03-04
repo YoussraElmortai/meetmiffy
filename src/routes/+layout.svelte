c7d7ec83218953d2e03414ec4a4325335cb95d6a
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2026-03-04 06:56:59 +0100

    setting up 🏠

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<header>
+	<h1>Meet miffy</h1>
+</header>
+
+{@render children()}
+
+<footer></footer>
